73fe193bf5b2bf95057e64740a173fb4fbf1f35e
Greg Weng <snowmantw@gmail.com>
2013-12-11 09:49:34 +0800

    Update

--- tasks/task.js
+++ tasks/task.js
@@ -2,32 +2,33 @@ var fs = require('fs'),
     grunt = require('grunt'),
     Builder = require('./lib/gaia-builder.js');
 
-grunt.registerTask('gaiabuilder', function() {
+module.exports = function(grunt) {
+  grunt.task.registerMultiTask('gaiabuilder', function() {
 
-  var opts = this.options({
-    'depends': [],
-    'essentialPath': '/tmp/test-build',
-    'profilePath': '/tmp/test-profile'
+    var opts = this.options({
+      'depends': [],
+      'essentialPath': '/tmp/test-build',
+      'profilePath': '/tmp/test-profile'
+    });
+    var done = this.async();
+
+    if (! fs.existsSync(opts.profilePath))
+      fs.mkdirSync(opts.profilePath);
+
+    Builder(opts)
+    .cloneEssential()
+    .trace('cloneEssential')
+    .makeDeps()
+    .trace('makeDeps')
+    .cloneDeps()
+    .trace('cloneDeps')
+    .buildDeps()
+    .trace('buildDeps')
+    .buildProfile()
+    .trace('buildProfile')
+    .fn(function(){
+      done();
+    })
+    .done();
   });
-  var done = this.async();
-
-  if (! fs.existsSync(opts.profilePath))
-    fs.mkdirSync(opts.profilePath);
-
-  Builder(opts)
-  .cloneEssential()
-  .trace('cloneEssential')
-  .makeDeps()
-  .trace('makeDeps')
-  .cloneDeps()
-  .trace('cloneDeps')
-  .buildDeps()
-  .trace('buildDeps')
-  .buildProfile()
-  .trace('buildProfile')
-  .fn(function(){
-    done();
-  })
-  .done();
-});
-
+};
